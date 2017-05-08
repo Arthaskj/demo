56a6e953c37a27d33c0133f00e7f4b17c3cd58c1
userkejun <1285707427@qq.com>
2017-05-08 22:16:05 +0800

    linux
    我们的

---
+++ laotie/dongrui/js/dr.js
@@ -0,0 +1,290 @@
+window.onload=function(){
+	var b1=document.getElementsByClassName("box1")[0];
+	var b2=document.getElementsByClassName("box2")[0];
+	var b3=document.getElementsByClassName("box3")[0];
+	var b4=document.getElementsByClassName("box4")[0];
+	var b5=document.getElementsByClassName("box5")[0];
+	var b6=document.getElementsByClassName("box6")[0];
+	var b7=document.getElementsByClassName("box7")[0];
+	var b8=document.getElementsByClassName("box8")[0];
+	var b9=document.getElementsByClassName("box9")[0];
+	var ba=document.getElementsByClassName("boxa")[0];
+	var bb=document.getElementsByClassName("boxb")[0];
+	var bc=document.getElementsByClassName("boxc")[0];
+	var bd=document.getElementsByClassName("boxd")[0];
+	var be=document.getElementsByClassName("boxe")[0];
+
+	var b11=document.getElementsByClassName("box11")[0];
+	var b22=document.getElementsByClassName("box22")[0];
+	var b33=document.getElementsByClassName("box33")[0];
+	var b44=document.getElementsByClassName("box44")[0];
+	var b55=document.getElementsByClassName("box55")[0];
+	var b66=document.getElementsByClassName("box66")[0];
+	var b77=document.getElementsByClassName("box77")[0];
+	var b88=document.getElementsByClassName("box88")[0];
+	var b99=document.getElementsByClassName("box99")[0];
+	var baa=document.getElementsByClassName("boxaa")[0];
+	var bbb=document.getElementsByClassName("boxbb")[0];
+	var bcc=document.getElementsByClassName("boxcc")[0];
+	var bdd=document.getElementsByClassName("boxdd")[0];
+	var bee=document.getElementsByClassName("boxee")[0];
+
+
+	b11.onmouseover=function (){
+		b11.style.transform="translateX(250px)";
+		b11.style.opacity="0";
+	}
+	b11.onmouseout=function (){
+		b11.style.transform="translateX(0px)";
+		b11.style.opacity="1";
+	}
+	b1.onmouseover=function (){
+		b11.style.transform="translateX(250px)";
+		b11.style.opacity="0";
+	}
+	b1.onmouseout=function (){
+		b11.style.transform="translateX(0px)";
+		b11.style.opacity="1";
+	}
+
+	b22.onmouseover=function (){
+		b22.style.transform="translateX(250px)";
+		b22.style.opacity="0";
+	}
+	b22.onmouseout=function (){
+		b22.style.transform="translateX(0px)";
+		b22.style.opacity="1";
+	}
+	b2.onmouseover=function (){
+		b22.style.transform="translateX(250px)";
+		b22.style.opacity="0";
+	}
+	b2.onmouseout=function (){
+		b22.style.transform="translateX(0px)";
+		b22.style.opacity="1";
+	}
+
+	b33.onmouseover=function (){
+		b33.style.transform="translateX(250px)";
+		b33.style.opacity="0";
+	}
+	b33.onmouseout=function (){
+		b33.style.transform="translateX(0px)";
+		b33.style.opacity="1";
+	}
+	b3.onmouseover=function (){
+		b33.style.transform="translateX(250px)";
+		b33.style.opacity="0";
+	}
+	b3.onmouseout=function (){
+		b33.style.transform="translateX(0px)";
+		b33.style.opacity="1";
+	}
+
+	b44.onmouseover=function (){
+		b44.style.transform="translateX(250px)";
+		b44.style.opacity="0";
+	}
+	b44.onmouseout=function (){
+		b44.style.transform="translateX(0px)";
+		b44.style.opacity="1";
+	}
+	b4.onmouseover=function (){
+		b44.style.transform="translateX(250px)";
+		b44.style.opacity="0";
+	}
+	b4.onmouseout=function (){
+		b44.style.transform="translateX(0px)";
+		b44.style.opacity="1";
+	}
+
+b55.onmouseover=function (){
+		b55.style.transform="translateX(250px)";
+		b55.style.opacity="0";
+	}
+	b55.onmouseout=function (){
+		b55.style.transform="translateX(0px)";
+		b55.style.opacity="1";
+	}
+	b5.onmouseover=function (){
+		b55.style.transform="translateX(250px)";
+		b55.style.opacity="0";
+	}
+	b5.onmouseout=function (){
+		b55.style.transform="translateX(0px)";
+		b55.style.opacity="1";
+	}
+
+	b66.onmouseover=function (){
+		b66.style.transform="translateX(250px)";
+		b66.style.opacity="0";
+	}
+	b66.onmouseout=function (){
+		b66.style.transform="translateX(0px)";
+		b66.style.opacity="1";
+	}
+	b6.onmouseover=function (){
+		b66.style.transform="translateX(250px)";
+		b66.style.opacity="0";
+	}
+	b6.onmouseout=function (){
+		b66.style.transform="translateX(0px)";
+		b66.style.opacity="1";
+	}
+
+	b77.onmouseover=function (){
+		b77.style.transform="translateX(250px)";
+		b77.style.opacity="0";
+	}
+	b77.onmouseout=function (){
+		b77.style.transform="translateX(0px)";
+		b77.style.opacity="1";
+	}
+	b7.onmouseover=function (){
+		b77.style.transform="translateX(250px)";
+		b77.style.opacity="0";
+	}
+	b7.onmouseout=function (){
+		b77.style.transform="translateX(0px)";
+		b77.style.opacity="1";
+	}
+
+b88.onmouseover=function (){
+		b88.style.transform="translateX(250px)";
+		b88.style.opacity="0";
+	}
+	b88.onmouseout=function (){
+		b88.style.transform="translateX(0px)";
+		b88.style.opacity="1";
+	}
+	b8.onmouseover=function (){
+		b88.style.transform="translateX(250px)";
+		b88.style.opacity="0";
+	}
+	b8.onmouseout=function (){
+		b88.style.transform="translateX(0px)";
+		b88.style.opacity="1";
+	}
+
+b99.onmouseover=function (){
+		b99.style.transform="translateX(250px)";
+		b99.style.opacity="0";
+	}
+	b99.onmouseout=function (){
+		b99.style.transform="translateX(0px)";
+		b99.style.opacity="1";
+	}
+	b9.onmouseover=function (){
+		b99.style.transform="translateX(250px)";
+		b99.style.opacity="0";
+	}
+	b9.onmouseout=function (){
+		b99.style.transform="translateX(0px)";
+		b99.style.opacity="1";
+	}
+
+baa.onmouseover=function (){
+		baa.style.transform="translateX(250px)";
+		baa.style.opacity="0";
+	}
+	baa.onmouseout=function (){
+		baa.style.transform="translateX(0px)";
+		baa.style.opacity="1";
+	}
+	ba.onmouseover=function (){
+		baa.style.transform="translateX(250px)";
+		baa.style.opacity="0";
+	}
+	ba.onmouseout=function (){
+		baa.style.transform="translateX(0px)";
+		baa.style.opacity="1";
+	}
+
+bbb.onmouseover=function (){
+		bbb.style.transform="translateX(250px)";
+		bbb.style.opacity="0";
+	}
+	bbb.onmouseout=function (){
+		bbb.style.transform="translateX(0px)";
+		bbb.style.opacity="1";
+	}
+	bb.onmouseover=function (){
+		bbb.style.transform="translateX(250px)";
+		bbb.style.opacity="0";
+	}
+	bb.onmouseout=function (){
+		bbb.style.transform="translateX(0px)";
+		bbb.style.opacity="1";
+	}
+
+bcc.onmouseover=function (){
+		bcc.style.transform="translateX(250px)";
+		bcc.style.opacity="0";
+	}
+	bcc.onmouseout=function (){
+		bcc.style.transform="translateX(0px)";
+		bcc.style.opacity="1";
+	}
+	bc.onmouseover=function (){
+		bcc.style.transform="translateX(250px)";
+		bcc.style.opacity="0";
+	}
+	bc.onmouseout=function (){
+		bcc.style.transform="translateX(0px)";
+		bcc.style.opacity="1";
+	}
+
+bdd.onmouseover=function (){
+		bdd.style.transform="translateX(250px)";
+		bdd.style.opacity="0";
+	}
+	bdd.onmouseout=function (){
+		bdd.style.transform="translateX(0px)";
+		bdd.style.opacity="1";
+	}
+	bd.onmouseover=function (){
+		bdd.style.transform="translateX(250px)";
+		bdd.style.opacity="0";
+	}
+	bd.onmouseout=function (){
+		bdd.style.transform="translateX(0px)";
+		bdd.style.opacity="1";
+	}
+
+	bee.onmouseover=function (){
+		bee.style.transform="translateX(250px)";
+		bee.style.opacity="0";
+	}
+	bee.onmouseout=function (){
+		bee.style.transform="translateX(0px)";
+		bee.style.opacity="1";
+	}
+	be.onmouseover=function (){
+		bee.style.transform="translateX(250px)";
+		bee.style.opacity="0";
+	}
+	be.onmouseout=function (){
+		bee.style.transform="translateX(0px)";
+		bee.style.opacity="1";
+	}
+
+
+var aa=document.getElementsByClassName("aa")[0];
+
+	var bbt=true;
+	aa.onclick=function(){
+		if (bbt)
+		{
+			
+			myMusic.play();
+			bbt=false;
+		}else{
+			
+			myMusic.pause();
+			bbt=true;		
+		}	
+	}
+
+
+
+	}
